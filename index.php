dd8b376dd98de8f86e1045988ee30dc6df2bf08e
LorenzoTesoro <lorenzotesoro2@gmail.com>
2022-12-12 17:03:48 +0100

    add fixed styles

--- index.php
+++ index.php
@@ -30,6 +30,13 @@ require __DIR__ . '/db.php';
     <!-- Bootstrap CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
+
+
+    <style>
+        .row {
+            flex-wrap: wrap;
+        }
+    </style>
 </head>
 
 <body class="bg-dark text-light">
@@ -37,7 +44,7 @@ require __DIR__ . '/db.php';
     <h1 class="text-center mt-4 mb-4">Products</h1>
 
     <div class="container pt-3">
-        <div class="row">
+        <div class="row row-cols-4 gy-4">
             <?php foreach ($products as $product) : ?>
                 <div class="col">
                     <div class="card p-2 bg-primary">
